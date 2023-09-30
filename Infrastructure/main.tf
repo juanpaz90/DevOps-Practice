15f63106d66dd58f6c3e7f3a4415f3ae82b296e5
juanpaz90 <juan.pazmino@thoughtworks.com>
2023-09-30 18:21:09 -0500

    K8s deployment file

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,5 +1,5 @@
 resource "google_container_cluster" "my_cluster" {
-  name               = "my-gke-cluster"
+  name               = "django-app-cluster"
   location           = "us-central1"
   initial_node_count = 1
   node_config {
